1e56a55f49fd4f4a2ea3d7b0952b1a6fc0e7d82d
Jia Hao Chin <jiahao.c@RUSHs-MacBook-Pro.local>
2024-12-17 18:18:06 +0800

    replace image and enhance seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,12 @@ const MyApp: AppType = ({ Component, pageProps }) => {
       <Head>
         <title>Appreciation | YWKL</title>
         <meta name="description" content="YW Appreciation Dinner" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="600" />
+        <meta property="og:image:height" content="600" />
+        <meta property="og:image:alt" content="You Got Mail!" />
+        <meta property="og:image:url" content="/yw_logo.png" />
       </Head>
       <NextUIProvider>
         <Component {...pageProps} />
